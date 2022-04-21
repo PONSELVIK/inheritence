a2b9587d10de7913439100cc52260cdda25608e0
PONSELVIK <99760844+PONSELVIK@users.noreply.github.com>
2022-04-21 21:53:40 +0530

    Add files via upload

---
+++ .index.js
@@ -0,0 +1,41 @@
+console.log("working");
+// Q.NO 1:
+function fun(){
+    console.log("this is function");
+}fun();
+
+//Q.NO 2:
+function sum1(a,b){
+    console.log("sum "+a+","+b+" is "+(a+b));
+}sum1(3,4);
+
+let arr=[5,6,7,9];
+let sum=0;
+arr.forEach((val)=>{
+    sum+=val;
+})
+console.log("sum of array elements is "+sum);
+
+//Q4.
+ undefined
+
+//Q.5
+ 21
+ undefined
+
+//Q6.
+// ReferenceError: Cannot access 'a' before initialization
+
+//Q.7
+function factorial(n){
+    let fact = 1;
+    if (n == 0 || n == 1){
+      return fact;
+    }else{
+      for(var i = n; i >= 1; i--){
+        fact = fact * i;
+      }
+      return fact;
+    }  
+  }
+console.log(factorial(4));
